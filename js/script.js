3a35d04826030cfdec79e255859fd8c6d52795ad
AlexanderKaminsky <kaminskyalexanderv@gmail.com>
2021-12-01 12:42:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+// let number = 4.6;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = `10`;
+// console.log(persone);
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     ismarried: false
+// };
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[5]);
+
+// alert("Hello, you don't save information!");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// // console.log(answer * 10);
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// // answers[0] = prompt("Как Ваше имя?", "");
+// // answers[1] = prompt("Как Ваша фамилия?", "");
+// // answers[2] = prompt("Сколько Вам лет?", "");
+
+// // document.write(answers);
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+// console.log(typeof true);
+// console.log(typeof null);
+// typeof document.all;
+
+// const category = 'toys';
+// console.log('https://someurl.com/' + category + '/' + '5');
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Alexander Kaminsky";
+// alert(`Hello, ${user}!`);
+// // console.log(typeof(user));
+
+// // console.log('arr' + ' - object');
+// // console.log(4 + 5);
+
+// let incr = 10,
+//     decr = 10;
+
+//     ++incr;
+//     --decr;
+
+//     console.log(incr);
+//     console.log(decr);
+
+//     console.log(incr++);
+//     console.log(decr--);
+    
+    
+    console.log(15%4);
+
+    console.log(2*3 === 8);  
+
+    const isChecked = true,
+        isClose = true;
+
+    console.log(isChecked && isClose);
+
+    console.log(26%5);
+
+
